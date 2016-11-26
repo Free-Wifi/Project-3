1ffe0be8b8fc932110274dd8f8be340c1be8e015
mflavin <mflavin@hawk.iit.edu>
2016-11-25 22:38:57 -0600

    Need new api, removed old one.

--- site.js
+++ site.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-  $('time-form').on('submit', function(event) {
+  $('#time-form').on('submit', function(event) {
     var loc = $('location').val();
     $.get(
-      'https://www.amdoren.com/api/timezone.php?api_key=EPubJusTVMR6qDABsQdcXEVBqD4qad&loc=' + loc,
+
       function(data) {
         $('#primary').append(
-          '<p><a href="'+data.time+'"></a></p>';
+
         );
       });
       event.preventDefault();
