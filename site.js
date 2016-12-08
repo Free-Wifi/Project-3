b92b91f6b575be61c3b39d2fd815842a32d0e921
mflavin <mflavin@hawk.iit.edu>
2016-12-08 01:08:06 -0600

    Added state to be returned

--- site.js
+++ site.js
@@ -5,7 +5,8 @@ $(document).ready(function() {
       'https://api.wunderground.com/api/12e40bb7b66b204b/conditions/q/'+loc+'.json',
       function(data) {
         $('#primary').append(
-          '<p><a href="'+data.current_observation.forecast_url+'">'+data.current_observation.display_location.city+'</a></p>'
+          '<p><a href="'+data.current_observation.forecast_url+'">'+data.current_observation.display_location.city+', '
+                        +data.current_observation.display_location.state+'</a></p>'
         );
       });
       event.preventDefault();
